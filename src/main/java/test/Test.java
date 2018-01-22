3dbe05a76ff86d3541af14a3bb587a8f98d27b35
javacoffice <java_coffice@163.com>
2018-01-22 16:25:40 +0800

    aaasdsds

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,11 +4,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("AAA");
-        System.out.println("AAA");
-        System.out.println("AAA");
-        System.out.println("AAA");
-        System.out.println("AAA");
-        System.out.println("AAA");
     }
 
 }
